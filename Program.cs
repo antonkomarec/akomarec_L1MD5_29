f49aefa0c668df41438c5f990e91fa346b1e23df
antonkomarec <95423359+antonkomarec@users.noreply.github.com>
2022-02-06 21:43:34 +0200

    ПОСЛЕДНЯЯ РЕДАКЦИЯ

--- Program.cs
+++ Program.cs
@@ -7,82 +7,147 @@ namespace akomarec_L1MD5_29
     {
         static void Main(string[] args)
         {
+            int nomer; // Номер в алфавите
+            int d; // Смещение
+            string s; //Результат
+            int j, f; // Переменная для циклов
+            int t = 0; // Преременная для нумерации символов ключа.
+            
 
-            //char[] tabI = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-            //char[] tabJ = new char[tabI.Length];
-            //for (int i = 0; i < tabI.Length; i++)
-            //{
-            //    int k = i+1;
-            //    if (k > tabI.Length-1)
-            //    {
-            //        k = k - tabI.Length;
-            //    }
-            //    tabJ[i] = tabI[k];
-            //}
-            //foreach (char c in tabJ)
-            //{
-            //    Console.WriteLine(c);
-
-            //}
-            char[,] tabNumber = new char [26, 26];
-            for (int j = 0; j < 26; j++)
+            Console.WriteLine("Введите СТРОЧНЫМИ СИМВОЛАМИ текст для шифрования:");
+            string text = Console.ReadLine();
+            char [] massage = text.ToCharArray(); // создаем массив символов сообщения
+            Console.WriteLine("Введите СТРОЧНЫМИ СИМВОЛАМИ ключ из не менее чем 5 символов:");
+            string k = Console.ReadLine();
+            var n = k.Length;
+            while (n < 5)
             {
-                for (int i = 0; i < 26; i++)
+                Console.WriteLine("Введите СТРОЧНЫМИ СИМВОЛАМИ ключ из не менее чем 5 символов:");
+                string k1 = Console.ReadLine();
+                n = k1.Length;
+                k = k1;
+            }
+            char [] key = k.ToCharArray(); // создаём массив символов ключа
+            char[] alfavit = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' }; // создаём массив символов алфавита
+
+            
+            for (int i = 0; i < massage.Length; i++) // Перебираем каждый символ сообщения
+            {
+                // Ищем индекс буквы
+                for (j = 0; j < alfavit.Length; j++) // Ищем индекс символа сообщения
+
                 {
-                    int k = i + j + 65;
-                    if (k > 90)
+                    if (massage[i] == alfavit[j]) // сравниваем с алфавитом
                     {
-                        tabNumber[i, j] = (char)(k - 26);
-                    }
-                    else 
-                    { 
-                        tabNumber[i, j] = (char)(i + j + 65); 
+                        break;
                     }
                 }
-            }
-            Console.WriteLine("Введите текст для шифрования:");
-            string text = Console.ReadLine();
-            var textArray = text.ToCharArray();
-            Console.WriteLine("Введите ключ из не менее чем 10 символов:");
-            string key = Console.ReadLine();
-            var n = key.Length;
-            while (n < 10)
-            {
-                Console.WriteLine("Введите ключ из не менее чем 10 символов:");
-                string key1 = Console.ReadLine();
-                n = key1.Length;
-                key = key1;
-            }
-            var keyArray = key.ToCharArray();
-            Console.WriteLine(key);
-            Console.WriteLine(key.Length);
-            char[] cipherKey = new char[26];
-            cipherKey.
-            for (int i = 0; i < 26; i++)
-            {
 
-                if (i <= key.Length-1)
+                if (j != 26) // Если j равно 26, значит символ не из алфавита
                 {
-                    cipherKey[i] = keyArray[i];
+                    nomer = j; // Индекс буквы
 
-                    //if (i%key.Length == 0)
-                    //{
-                    //   int k = key.Length;
-                    //}
+                    
+                    if (t > key.Length - 1) { t = 0; } // Ключ закончился - начинаем сначала.
 
+                    
+                    for (f = 0; f < alfavit.Length; f++) // Ищем индекс буквы ключа
+                    {
+                        if (key[t] == alfavit[f]) // сравниваем с алфавитом
+                        {
+                            break;
+                        }
+                    }
+
+                    t++;
+
+                    if (f != 26) // Если f равно 26, значит символ не из алфавита
+                    {
+                        d = nomer + f;
+                    }
+                    else
+                    {
+                        d = nomer;
+                    }
+                    
+                    if (d > 25) // Проверяем, чтобы не вышли за пределы алфавита
+                    {
+                        d = d - 26;
+                    }
+
+                    massage[i] = alfavit[d]; // Меняем букву на зашифрованную
                 }
                 else
                 {
-                    int k = 10;
-                    if (i % key.Length == 0)
-                    cipherKey[i] = keyArray[i - k];
+                    t++; // не забываем про инкремент
                 }
             }
+
+            s = new string(massage); // Собираем символы обратно в строку.
+            Console.WriteLine($"Зашифрованниое сообщение: {s}");
+
+            // -----------------------расшифровка сообщения-----------------------------------
+            char[,] tabNumber = new char[26, 26]; // создаём двумерный массив Таблицы Виженера
+            for (int j1 = 0; j1 < 26; j1++)
+            {
+                for (int i1 = 0; i1 < 26; i1++)
+                {
+                    int k1 = i1 + j1 + 65;
+                    if (k1 > 90)
+                    {
+                        tabNumber[i1, j1] = (char)(k1 - 26);
+                    }
+                    else
+                    {
+                        tabNumber[i1, j1] = (char)(i1 + j1 + 65);
+                    }
+                }
+            }
+
+            char[] cipher = s.ToCharArray(); // разбираем строку зашифрованного сообщения обратно в массив символов
+            t = 0;
             
-                Console.WriteLine(cipherKey);
-            
+            for (int i = 0; i < cipher.Length; i++) // Перебираем каждый символ ключа по кругу. Количество прогонов равно количеству символов шифра.
+            {
+                if (t > key.Length - 1) { t = 0; } // Ключ закончился - начинаем сначала.
+                
+                for (j = 0; j < alfavit.Length; j++) // Ищем индекс буквы ключа
+                {
+                    if (key[t] == alfavit[j])
+                    {
+                        break;
+                    }
+                 
+                }
+                t++;
+                int kodLiter;
+                for ( kodLiter = 0; kodLiter < 26; kodLiter++)
+                {
+                    if (tabNumber[j, kodLiter] == cipher[i]) // перебираем по столбцам (J)
+                                                             // и по строкам (kodLiter) таблицы Виженера
+                                                             // символы зашифрованного сообщения
+                    {
+                        break;
+                    }
+ 
+                }
+                if (kodLiter != 26) // Если f равно 26, значит символ не из алфавита
+                {
+                    massage[i] = alfavit[kodLiter];
+                    
+                }
+                else
+                {
+                    massage[i] = cipher[i]; // иначе - переписываем символ "как есть"
+                }
+
+            }
+            s = new string(massage); // собираем расшифрованное сообщение в строку
+            Console.WriteLine($"Расшифрованное сообщение: {s}");
+
             Console.ReadLine();
-            
+
         }
     }
 }
+// как-то так))), инерфейсы, формы и др. я ещё не учил...
